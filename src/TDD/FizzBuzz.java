1431cf8610c5e97928267f510626c1867691e5ff
fixza10101 <fixza101@gmail.com>
2019-10-03 18:18:35 +0700

    FizzBuzzCode_InputIs1_ExpectIs1

--- src/TDD/FizzBuzz.java
+++ src/TDD/FizzBuzz.java
@@ -5,38 +5,19 @@ import java.util.Scanner;
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-			Scanner key = new Scanner(System.in);
-			int input = key.nextInt();
-			if(input > 0 && input <=100) {
-				if(input % 3 == 0) {
-					String output = getFizz();
-				}else if(input % 5 == 0) {
-					String output = getBuzz();
-				}else if(input % 3 == 0 && input % 5 ==0) {
-					String output = getFizzBuzz();
-				}else {
-					int output = getNum(input);
-				}
-			}else {
-				getError();
-			}
-			
-		}
-		public static String getFizz() {
-			return "Fizz";
-		}
-		public static String getBuzz() {
-			return "Buzz";
-		}
-		public static String getFizzBuzz() {
-			return "FizzBuzz";
-		}
-		public static int getNum(int key) {
-			return key;
-		}
-		public static String getError() {
+		Scanner key = new Scanner(System.in);
+		int input = key.nextInt();
+		System.out.println(input);
+		String output = getInput(input);
+		
+		
+	}
+	public static String getInput(int input) {
+		if(input > 0 && input <=100) {
+			return Integer.toString(input);
+		}else {
 			return "Error";
 		}
-	
+	}
 
 }
